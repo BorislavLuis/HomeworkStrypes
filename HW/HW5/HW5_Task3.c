db0d257bf6cf983c55b9c1313156da3ff85f615e
BorislavLuis <54331824+BorislavLuis@users.noreply.github.com>
2022-08-27 13:30:31 +0300

    HW5

--- HW/HW5/HW5_Task3.c
+++ HW/HW5/HW5_Task3.c
@@ -2,10 +2,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-  
+    unsigned int n;
+    time_t t;
 
+    srand((unsigned)time(&t));
+
+    scanf("%u", &n);
+    float* f = (float*)malloc(n * sizeof(float));
+    for (int i = 0; i < n; i++)
+    {
+        f[i] = (float)rand()/(float)RAND_MAX;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%0.2f\n",f[i]);
+    }
+    free(f);
     return 0;
 }
