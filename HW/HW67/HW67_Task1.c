7a163ee621710b7093e7bbeb5314c6eda658ea6f
BorislavLuis <54331824+BorislavLuis@users.noreply.github.com>
2022-09-01 09:29:26 +0300

    HW67

---
+++ HW/HW67/HW67_Task1.c
@@ -0,0 +1,31 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <stdio.h>
+
+unsigned char CheckBit(unsigned int uValue);
+
+int main()
+{
+	unsigned int number = 2147483648;
+	printf("The number has exactly one bit set (1-true,0-false): %c", CheckBit(number));
+	return 0;
+}
+
+unsigned char CheckBit(unsigned int uValue)
+{
+	int n = sizeof(int)*8;
+	int times = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (uValue & 1 << i)
+		{
+			times++;
+		}
+	}
+	if (times == 1)
+	{
+		return '1';
+	}
+	return '0';
+}
