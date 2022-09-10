e08cbbcb675a8560eed1cc47281a5634e6b1fce2
BorislavLuis <54331824+BorislavLuis@users.noreply.github.com>
2022-09-10 15:28:29 +0300

    Final

--- FINAL TEST/FINAL/Task1/Task1.c
+++ FINAL TEST/FINAL/Task1/Task1.c
@@ -8,16 +8,32 @@
 int main()
 {
 	//srand(time(NULL));
-	int n,m,k;
-	scanf_s("%d", &n);
+	unsigned int n,m,k;
+	scanf("%u", &n);
 	char* arr = malloc(n*sizeof(char));
-	strcpy(arr, "bo\0");
-	//for (int i = 0; i < n; i++)
-	//{
-	//	//char c = (char)rand(65, 90);
-	//	//*arr = 'j';
-	//	//strcat(*arr,c);
-	//}
+    for (int i = 0; i < n; i++)
+    {
+        char c = (char)rand() % ('z' - 'a') + 'a';
+        arr[i] = c;
+    }
+    scanf("%u", &m);
+    arr = realloc(arr, (n + m) * sizeof(char));
+    for (int i = n; i < m + n; i++)
+    {
+        char c = (char)rand() % ('Z' - 'A') + 'A';
+        arr[i] = c;
+    }
+    scanf("%u", &k);
+    arr = realloc(arr, (n + m + k) * sizeof(char));
+    for (int i = n + m; i < m + n + k; i++)
+    {
+        char c = (char)rand() % ('-' - '!') + '!';
+        arr[i] = c;
+    }
+    for (int i = 0; i < n + m + k; i++)
+    {
+        printf("%c\n", arr[i]);
+    }
 	free(arr);
 	return 0;
 }
