7ec9624f4eb72ee91411c88607b7fcf5f4c5a09f
BorislavLuis <54331824+BorislavLuis@users.noreply.github.com>
2022-09-10 14:02:34 +0300

    Final

--- FINAL TEST/FINAL/Task1/Task1.c
+++ FINAL TEST/FINAL/Task1/Task1.c
@@ -2,8 +2,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
 
 int main()
 {
+	//srand(time(NULL));
+	int n,m,k;
+	scanf_s("%d", &n);
+	char* arr = malloc(n*sizeof(char));
+	strcpy(arr, "bo\0");
+	//for (int i = 0; i < n; i++)
+	//{
+	//	//char c = (char)rand(65, 90);
+	//	//*arr = 'j';
+	//	//strcat(*arr,c);
+	//}
+	free(arr);
 	return 0;
 }
